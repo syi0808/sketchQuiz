0239f2b4428d3e8f63c5178e59d60b9b486a54bb
Sung Ye In <syi9397@naver.com>
2021-04-09 08:51:34 +0900

    create lobby component

---
+++ client/src/components/Lobby/Lobby.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+const Lobby = () => {
+    return (
+        <>
+            <div>asd</div>
+        </>
+    );
+};
+
+export default Lobby;
